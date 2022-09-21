3dc448bcdd5c772530765efb650b9b34676a2225
KunalMehra075 <112753481+KunalMehra075@users.noreply.github.com>
2022-09-21 09:11:56 +0530

    modified_code

--- prime.js
+++ prime.js
@@ -1,15 +1,20 @@
-function checkPrime(num){
-  let count=0;
-    for(i=1;i<=num;i++){
-        if(num%i==0){
-            count++;
+function checkPrime(number){
+  let factors=0;
+    for(i=1;i<=number;i++){
+        if(number%i===0){
+            factors++;
         }
     }
-    if(count==2){
+    if(factors==2){
         return true;
     }
     return false;
 }
 
-let A = checkPrime(17)
-console.log(A)
+let A = checkPrime(17);
+if(A==true){
+    console.log("Prime")
+}else(
+    console.log("Not a Prime")
+)
+
